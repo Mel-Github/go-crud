6686e5c9a96e77bedece38f1329ef88962bdc7ce
Mel-GitHub <mel000vin@yahoo.com>
2020-11-29 22:57:16 +0800

    First commit

---
+++ client/cmd/root.go
@@ -0,0 +1,124 @@
+/*
+Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+	"context"
+	"time"
+	"log"
+
+	homedir "github.com/mel-github/go-crud"
+	"github.com/spf13/viper"
+	blogpb "github.com/mel-github/go-crud/proto"
+	//blogpb "../../proto"
+	"google.golang.org/grpc"
+)
+
+var cfgFile string
+
+// Client and context global vars
+var client blogpb.BlogServiceClient
+var requestCtx context.Context
+var requestOpts grpc.DialOption
+var cancel context.CancelFunc
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "client",
+	Short: "A brief description of your application",
+	Long: `A longer description that spans multiple lines and likely contains
+examples and usage of using your application. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	//	Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.client.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	// We initialize the client
+	fmt.Println("Starting Blog Service Client")
+	// Establish context to timeout if server does not respond
+	requestCtx, cancel = context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	// Establish insecure grpc options (no TLS)
+	requestOpts = grpc.WithInsecure()
+	// Dial the server, returns a client connection
+	conn, err := grpc.Dial("localhost:50051", requestOpts)
+	if err != nil {
+		log.Fatalf("Unable to establish client connection to localhost:50051: %v", err)
+	}
+
+	// defer posptones the execution of a function until the surrounding function returns
+	// conn.Close() will not be called until the end of main()
+	// The arguments are evaluated immeadiatly but not executed
+	// defer conn.Close()
+
+	// Instantiate the BlogServiceClient with our client connection to the server
+	client = blogpb.NewBlogServiceClient(conn)
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// Search config in home directory with name ".client" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".client")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
